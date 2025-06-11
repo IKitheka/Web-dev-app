d012da021ef7e7122c891cd6cf4be370b7b4b5c2
LNgatia <leon.ngatia@strathmore.edu>
2025-06-11 13:23:15 +0300

    new commit

--- Application/database/connection.php
+++ Application/database/connection.php
@@ -1,23 +1,24 @@
 <?php
+
 $host = getenv("DB_HOST") ?? 'intern-db';
 $port =  getenv("DB_PORT") ?? '5432';
 $dbname =  getenv("DB_NAME") ?? 'intern-db';
 $username =  getenv("DB_USER") ?? 'intern-user';
 $password =  getenv("DB_PASS") ?? 'intern-pass';
 
-try {
+function create_connection(){
+    global $host, $port, $dbname, $username, $password;
 
-    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
-    $pdo = new PDO($dsn, $username, $password);
-    
-    // Set PDO attributes
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-    
-} catch (PDOException $e) {
-    error_log("Database connection failed: " . $e->getMessage());
-    
-    die("Database connection failed: " . $e->getMessage());
+    $connection_string = "host=$host port=$port dbname=$dbname user=$username password=$password";
+    $connection = pg_connect($connection_string);
+    if (!$connection) {
+        die("Connection failed");
+    }
+
+    return $connection;
 }
+
+create_connection();
+
 
 ?>
